8aacb92b6723f734247a4124191870453b1509fd
rafal11ck <rafal11ck@users.noreply.github.com>
2021-01-09 19:17:08 +0100

    dragtest

--- ui/ui test2.lua
+++ ui/ui test2.lua
@@ -42,7 +42,7 @@ function library:CreateWindow(nazwa, x, y)
 	topper.Name = "topper"
 	topper.Parent = ScreenGui
 	topper.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
-	topper.Position = UDim2.new(0.5, 0, 0, -20)
+	topper.Position = body.UDim2 + UDim2.new(0, 0, 0, -20)
 	topper.Size = UDim2.new(0, x, 0, 20)
 	topper.BorderColor3 = Color3.new(bordcol)
 
@@ -81,9 +81,57 @@ function library:CreateWindow(nazwa, x, y)
 			hider.Rotation = 90
 		end	
 	end)
+	
+	-------------------------------------------------------------------------------------------------------------------
+	
+	local function dragscript() -- ScreenGui.dragscript 
+		local script = Instance.new('LocalScript', ScreenGui)
+
+		local UserInputService = game:GetService("UserInputService")
+		
+		local gui = script.Parent
+		
+		local dragging
+		local dragInput
+		local dragStart
+		local startPos
+		
+		local function update(input)
+			local delta = input.Position - dragStart
+			gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		end
+		
+		gui.InputBegan:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+				dragging = true
+				dragStart = input.Position
+				startPos = gui.Position
+		
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragging = false
+					end
+				end)
+			end
+		end)
+		
+		gui.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				dragInput = input
+			end
+		end)
+		
+		UserInputService.InputChanged:Connect(function(input)
+			if input == dragInput and dragging then
+				update(input)
+			end
+		end)
+	end
+	coroutine.wrap(dragscript)()
+	
+	------------------------------------------------------------------------------------------------------------------------------------------
 
 	local nooblib={}
-	
 	
 	-- classese ?
 	function library:CreateButton(nazwa, callback)
