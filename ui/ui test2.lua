6b738ca75a1360fa7a832c627db96247f0009df0
rafal11ck <rafal11ck@users.noreply.github.com>
2021-01-16 19:05:50 +0100

    Update ui test2.lua

--- ui/ui test2.lua
+++ ui/ui test2.lua
@@ -1,4 +1,4 @@
-local wersja = "3.15"
+local wersja = "3.15.2"
 print("UI "..wersja.."   by ciabar9ck#8155")  -- se printuje wersje 
 
 -- library:CreateToggle("testtog1", function(state)   -- nazwa zmienic _G.  zmienną 
@@ -21,8 +21,8 @@ function library:CreateWindow(nazwa, xpos, ypos) -- nazwa rozmiar pozycja
 	local xposoff = 0
 	local yposoff = 0
 	
-	x = x or 200
-	y= y or 400
+	x = x or 0
+	y= y or 0
 	if xpos ~= nil and xpos > 5 then -- pozycja x
 			xposoff = xpos
 			xpos = 0
