c619445f7d050d921e7d28e0fae8bd2dc4185c93
rafal11ck <rafal11ck@users.noreply.github.com>
2021-01-17 00:32:44 +0100

    Update ui test2.lua

--- ui/ui test2.lua
+++ ui/ui test2.lua
@@ -1,4 +1,4 @@
-local wersja = "3.16.2"
+local wersja = "3.16.3"
 print("UI "..wersja.."   by ciabar9ck#8155")  -- se printuje wersje 
 
 -- library:CreateToggle("testtog1", function(state)   -- nazwa zmienic _G.  zmienną 
@@ -250,7 +250,7 @@ function library:CreateWindow(nazwa, xpos, ypos) -- nazwa rozmiar pozycja
 		selected.Text = default or "Choose"
 		selected.Font = Enum.Font.SourceSans
 		
-		local openedswitchsize = 0
+		local openedswitchsize = UDim2.new(0, 0, 0, 0)
 		
 		local function openchoose()
 			switch:TweenSize(UDim2.new(0, 100, 0, openedswitchsize), "Out", "Linear", 0.2)
