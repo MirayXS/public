2d115c7e2d0a0e806cc84dd04d6e020a2558cb44
rafal11ck <rafal11ck@users.noreply.github.com>
2021-01-17 01:23:07 +0100

    Update ui test2.lua

--- ui/ui test2.lua
+++ ui/ui test2.lua
@@ -1,4 +1,4 @@
-local wersja = "3.16.10"
+local wersja = "3.16.11"
 print("UI "..wersja.."   by ciabar9ck#8155")  -- se printuje wersje 
 
 -- library:CreateToggle("testtog1", function(state)   -- nazwa zmienic _G.  zmienną 
@@ -277,7 +277,7 @@ function library:CreateWindow(nazwa, xpos, ypos) -- nazwa rozmiar pozycja
 		end
 		
 		local function openchoose()
-			print("opening")
+			print("opening" , openedswitchsize)
 			switch:TweenSize(UDim2.new(0, 100, 0, openedswitchsize), "Out", "Linear", 0.2)
 		end
 		
