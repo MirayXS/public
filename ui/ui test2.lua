823c0872c16398b53a22f9720d5317985735ec56
rafal11ck <rafal11ck@users.noreply.github.com>
2021-01-17 12:56:23 +0100

    Update ui test2.lua

--- ui/ui test2.lua
+++ ui/ui test2.lua
@@ -1,4 +1,4 @@
-local wersja = "3.16.25"
+local wersja = "3.16.25" -- 
 print("UI "..wersja.."   by ciabar9ck#8155")  -- se printuje wersje 
 
 -- library:CreateToggle("testtog1", function(state)   -- nazwa zmienic _G.  zmienną 
